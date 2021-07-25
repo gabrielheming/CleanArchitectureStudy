d3ecbd08a4878a786232b4fb23329fe2a576b462
Gabriel Bertolini Heming <gabriel.heming@hotmail.com>
2021-07-25 23:01:49 +0200

    Added IAsyncRepository

---
+++ src/Core/GloboTicket.TicketManagement.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Application.Contracts
+{
+    public interface IAsyncRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(Guid id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
