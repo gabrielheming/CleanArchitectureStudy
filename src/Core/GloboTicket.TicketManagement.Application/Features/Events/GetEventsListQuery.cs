7d01ed16f29c61e64599db61bc54d7f5a5459e52
Gabriel Bertolini Heming <gabriel.heming@hotmail.com>
2021-07-25 23:36:28 +0200

    Added GetEventListQuery request and handler

---
+++ src/Core/GloboTicket.TicketManagement.Application/Features/Events/GetEventsListQuery.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using MediatR;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events
+{
+    public class GetEventsListQuery : IRequest<List<EventListVm>>
+    {
+
+    }
+}
---
+++ src/Core/GloboTicket.TicketManagement.Application/Features/Events/GetEventsListQueryHandler.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts;
+using GloboTicket.TicketManagement.Domain.Entities;
+using MediatR;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events
+{
+    public class GetEventsListQueryHandler : IRequestHandler<GetEventsListQuery, List<EventListVm>>
+    {
+        private readonly IMapper mapper;
+        private readonly IAsyncRepository<Event> eventRepository;
+
+        public GetEventsListQueryHandler(IMapper mapper, IAsyncRepository<Event> eventRepository)
+        {
+            this.mapper = mapper;
+            this.eventRepository = eventRepository;
+        }
+
+        public async Task<List<EventListVm>> Handle(GetEventsListQuery request, CancellationToken cancellationToken)
+        {
+            IEnumerable<Event> allEvents = (await eventRepository.ListAllAsync()).OrderBy(_ => _.Date);
+            return mapper.Map<List<EventListVm>>(allEvents);
+        }
+    }
+}
