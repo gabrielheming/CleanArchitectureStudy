4c070fd330b4139dcba17941e5fecfaeaccd1ae9
Gabriel Bertolini Heming <gabriel.heming@hotmail.com>
2021-07-25 23:48:58 +0200

    Added GetEventDetail request and handler

---
+++ src/Core/GloboTicket.TicketManagement.Application/Features/Events/GetEventDetailQuery.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using MediatR;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events
+{
+    public class GetEventDetailQuery : IRequest<EventDetailVm>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ src/Core/GloboTicket.TicketManagement.Application/Features/Events/GetEventDetailQueryHandler.cs
@@ -0,0 +1,38 @@
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts;
+using GloboTicket.TicketManagement.Domain.Entities;
+using MediatR;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events
+{
+    public class GetEventDetailQueryHandler : IRequestHandler<GetEventDetailQuery, EventDetailVm>
+    {
+        private readonly IMapper mapper;
+        private readonly IAsyncRepository<Event> eventRepository;
+        private readonly IAsyncRepository<Category> categoryRepository;
+
+        public GetEventDetailQueryHandler(
+            IMapper mapper,
+            IAsyncRepository<Event> eventRepository,
+            IAsyncRepository<Category> categoryRepository)
+        {
+            this.mapper = mapper;
+            this.eventRepository = eventRepository;
+            this.categoryRepository = categoryRepository;
+        }
+
+        public async Task<EventDetailVm> Handle(GetEventDetailQuery request, CancellationToken cancellationToken)
+        {
+            Event @event = await eventRepository.GetByIdAsync(request.Id);
+            EventDetailVm eventDetailDto = mapper.Map<EventDetailVm>(@event);
+
+            Category category = await categoryRepository.GetByIdAsync(eventDetailDto.CategoryId);
+
+            eventDetailDto.Category = mapper.Map<CategoryDto>(category);
+
+            return eventDetailDto;
+        }
+    }
+}
