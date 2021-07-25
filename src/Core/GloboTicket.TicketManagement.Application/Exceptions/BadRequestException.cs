ab7c74b6fc92f20206601a978265fdf5d85ae904
Gabriel Bertolini Heming <gabriel.heming@hotmail.com>
2021-07-26 01:34:06 +0200

    Added Core/Application.Exceptions

---
+++ src/Core/GloboTicket.TicketManagement.Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GloboTicket.TicketManagement.Application.Exceptions
+{
+    public class BadRequestException : ApplicationException
+    {
+        public BadRequestException(string message) : base(message)
+        {
+
+        }
+    }
+}
