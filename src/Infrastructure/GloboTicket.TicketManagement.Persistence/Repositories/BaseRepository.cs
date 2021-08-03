d184cbce3f9abc248d760b1a08c6980b937a388d
Gabriel Bertolini Heming <gabriel.heming@hotmail.com>
2021-08-03 21:14:11 +0200

    Added BaseRepository implementation

---
+++ src/Infrastructure/GloboTicket.TicketManagement.Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GloboTicket.TicketManagement.Application.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace GloboTicket.TicketManagement.Persistence.Repositories
+{
+    public class BaseRepository<T> : IAsyncRepository<T> where T : class
+    {
+        protected readonly GloboTicketDbContext dbContext;
+
+        public BaseRepository(GloboTicketDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public virtual async Task<T> AddAsync(T entity)
+        {
+            await dbContext.Set<T>().AddAsync(entity);
+            await dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public virtual async Task DeleteAsync(T entity)
+        {
+            dbContext.Set<T>().Remove(entity);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public virtual async Task<T> GetByIdAsync(Guid id)
+        {
+            return await dbContext.Set<T>().FindAsync(id);
+        }
+
+        public virtual async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await dbContext.Set<T>().ToListAsync();
+        }
+
+        public virtual async Task UpdateAsync(T entity)
+        {
+            dbContext.Entry(entity).State = EntityState.Modified;
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
