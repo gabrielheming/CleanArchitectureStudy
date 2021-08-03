33ef258e9fa59ec633fc17532eae1713a40d5af0
Gabriel Bertolini Heming <gabriel.heming@hotmail.com>
2021-08-03 21:14:37 +0200

    Update IOrderRepository contract

--- src/Core/GloboTicket.TicketManagement.Application/Contracts/Persistence/IOrderRepository.cs
+++ src/Core/GloboTicket.TicketManagement.Application/Contracts/Persistence/IOrderRepository.cs
@@ -1,9 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using GloboTicket.TicketManagement.Domain.Entities;
 
 namespace GloboTicket.TicketManagement.Application.Contracts
 {
     public interface IOrderRepository : IAsyncRepository<Order>
     {
-
+        Task<List<Order>> GetPagedOrdersForMonth(DateTime date, int page, int size);
+        Task<int> GetTotalCountOfOrdersForMonth(DateTime date);
     }
 }
