7d14d85db97048ec208596a62b39438961d2b2cd
Gabriel Bertolini Heming <gabriel.heming@hotmail.com>
2021-08-03 21:45:37 +0200

    Implemented EmailService

---
+++ src/Infrastructure/GloboTicket.TicketManagement.Insfrastructure/Mail/EmailService.cs
@@ -0,0 +1,42 @@
+using System.Threading.Tasks;
+using GloboTicket.TicketManagement.Application.Contracts.Infrastructure;
+using GloboTicket.TicketManagement.Application.Models.Mail;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace GloboTicket.TicketManagement.Insfrastructure.Mail
+{
+    public class EmailService : IEmailService
+    {
+        private readonly EmailSettings emailSettings;
+
+        public EmailService(IOptions<EmailSettings> emailSettings)
+        {
+            this.emailSettings = emailSettings.Value;
+        }
+
+        public async Task<bool> SendEmailAsync(Email email)
+        {
+            SendGridClient client = new SendGridClient(emailSettings.ApiKey);
+
+            string subject = email.Subject;
+            EmailAddress to = new EmailAddress(email.To);
+            string emailBody = email.Body;
+
+            EmailAddress from = new EmailAddress
+            {
+                Email = emailSettings.FromAddress,
+                Name = emailSettings.FromName
+            };
+
+            SendGridMessage sendGridMessage = MailHelper.CreateSingleEmail(from, to, subject, emailBody, emailBody);
+            Response response = await client.SendEmailAsync(sendGridMessage);
+
+            return (
+                response.StatusCode == System.Net.HttpStatusCode.Accepted
+                || response.StatusCode == System.Net.HttpStatusCode.OK
+            );
+        }
+    }
+}
