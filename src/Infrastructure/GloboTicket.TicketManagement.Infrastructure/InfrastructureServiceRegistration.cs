02e7dcb31382e0239342de996df401f2f0adc38c
Gabriel Bertolini Heming <gabriel.heming@hotmail.com>
2021-08-08 17:39:28 +0200

    Renamed service registration extension method

--- src/Infrastructure/GloboTicket.TicketManagement.Infrastructure/InfrastructureServiceRegistration.cs
+++ src/Infrastructure/GloboTicket.TicketManagement.Infrastructure/InfrastructureServiceRegistration.cs
@@ -1,6 +1,6 @@
 using GloboTicket.TicketManagement.Application.Contracts.Infrastructure;
 using GloboTicket.TicketManagement.Application.Models.Mail;
-using GloboTicket.TicketManagement.Insfrastructure.Mail;
+using GloboTicket.TicketManagement.Infrastructure.Mail;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,7 +8,7 @@ namespace GloboTicket.TicketManagement.Infrastructure
 {
     public static class InfrastructureServiceRegistration
     {
-        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
             services.AddTransient<IEmailService, EmailService>();
