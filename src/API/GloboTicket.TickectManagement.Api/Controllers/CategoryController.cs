0f263cfcbc984c857d0edd98173e1fa5f3db7d39
Gabriel Bertolini Heming <gabriel.heming@hotmail.com>
2021-08-08 18:18:32 +0200

    Added category controller

---
+++ src/API/GloboTicket.TickectManagement.Api/Controllers/CategoryController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesList;
+using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesListWithEvents;
+using GloboTicket.TicketManagement.Application.Features.Events.Commands.CreateCategory;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GloboTicket.TickectManagement.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public CategoryController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet("all", Name = "GetAllCategories")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<List<CategoryListVm>>> GetAllCategories()
+        {
+            List<CategoryListVm> dtos = await mediator.Send(new GetCategoriesListQuery());
+            return Ok(dtos);
+        }
+
+        [HttpGet("allwithevents", Name = "GetAllCategoriesWithEvents")]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<List<CategoryEventListVm>>> GetAllCategoriesWithEvents(bool includeHistory)
+        {
+            List<CategoryEventListVm> dtos = await mediator.Send(new GetCategoriesListWithEventsQuery() { IncludePassedEvents = includeHistory });
+            return Ok(dtos);
+        }
+
+        [HttpPost(Name = "AddCategory")]
+        public async Task<ActionResult<CreateCategoryCommandResponse>> Create([FromBody] CreateCategoryCommand createCategoryCommand)
+        {
+            CreateCategoryCommandResponse response = await mediator.Send(createCategoryCommand);
+            return Ok(response);
+        }
+    }
+}
