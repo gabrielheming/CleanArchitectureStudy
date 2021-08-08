aea68c96412ed0b989035adf2a5d0a28e6a1812c
Gabriel Bertolini Heming <gabriel.heming@hotmail.com>
2021-08-08 17:40:22 +0200

    Registered the services

--- src/API/GloboTicket.TickectManagement.Api/Startup.cs
+++ src/API/GloboTicket.TickectManagement.Api/Startup.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using GloboTicket.TicketManagement.Application;
+using GloboTicket.TicketManagement.Infrastructure;
+using GloboTicket.TicketManagement.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace GloboTicket.TickectManagement.Api
@@ -23,9 +19,14 @@ namespace GloboTicket.TickectManagement.Api
 
         public IConfiguration Configuration { get; }
 
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddApplicationServices();
+            services.AddInfrastructureServices(Configuration);
+            services.AddPersistenceServices(Configuration);
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
