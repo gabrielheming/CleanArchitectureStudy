c9d2ca638c9f09c3c0e458b23c8f6baf4a04f332
Gabriel Bertolini Heming <gabriel.heming@hotmail.com>
2021-08-08 17:45:59 +0200

    Added CORS support

--- src/API/GloboTicket.TickectManagement.Api/Startup.cs
+++ src/API/GloboTicket.TickectManagement.Api/Startup.cs
@@ -33,6 +33,11 @@ namespace GloboTicket.TickectManagement.Api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "GloboTicket.TickectManagement.Api", Version = "v1" });
             });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Open", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +55,8 @@ namespace GloboTicket.TickectManagement.Api
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("Open");
 
             app.UseEndpoints(endpoints =>
             {
